907b01c1056f90bce642ab6cc5f5771c276e8a1f
vu-linh <138560515+vu-linh@users.noreply.github.com>
2024-07-05 15:37:16 +0700

    Upgrade spring boot version, testing new QueryDSL's annotation processor on maven-compiler-plugin

--- src/main/java/com/vulinh/SpringBaseProjectApplication.java
+++ src/main/java/com/vulinh/SpringBaseProjectApplication.java
@@ -4,9 +4,12 @@ import com.vulinh.configuration.AuditorConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = AuditorConfiguration.AUDITOR_PROVIDER)
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class SpringBaseProjectApplication {
 
   public static void main(String[] args) {
