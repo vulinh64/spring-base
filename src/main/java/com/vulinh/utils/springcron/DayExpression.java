90476d2107167d94bf5ce81be576b757de2de243
vu-linh <138560515+vu-linh@users.noreply.github.com>
2024-09-30 17:52:00 +0700

    Day expression bug fix

--- src/main/java/com/vulinh/utils/springcron/DayExpression.java
+++ src/main/java/com/vulinh/utils/springcron/DayExpression.java
@@ -21,9 +21,8 @@ public enum DayExpression implements DateExpression {
       ExpressionUtils::everyBetween),
   SPECIFIC_DAYS(
       list ->
-          Utils.isValidListWithinBounds(
-              list, Constant.MIN_DAY_MONTH_DAY_OF_WEEK, Constant.MAX_MONTH),
-      list -> ExpressionUtils.separateByComma(list, ExpressionUtils.MONTHS_MAPPING::get));
+          Utils.isValidListWithinBounds(list, Constant.MIN_DAY_MONTH_DAY_OF_WEEK, Constant.MAX_DAY),
+      ExpressionUtils::separateByComma);
 
   private final Predicate<List<Integer>> validator;
   private final Function<List<Integer>, String> generator;
