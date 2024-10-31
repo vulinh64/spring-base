bf37eafc325836ad71a33ee2e934fecd1ad77428
Nguyen Vu Linh <nguyenvulinh64@outlook.com>
2024-10-31 23:18:13 +0700

    Batch files that: if no container, create it. if container existed but is stopped, start it, otherwise inform users that container is already running (thank Claude AI)

--- run-elasticsearch-docker-windows.cmd
+++ run-elasticsearch-docker-windows.cmd
@@ -1,2 +1,18 @@
 @echo off
-docker run -d --name standalone-elasticsearch -e "xpack.security.enabled=false" -e "discovery.type=single-node" -p 9200:9200 -p 9300:9300 elasticsearch:8.15.2
+SET CONTAINER_NAME=standalone-postgresql
+SET COMMAND=docker run -d --name %CONTAINER_NAME% -e "xpack.security.enabled=false" -e "discovery.type=single-node" -p 9200:9200 -p 9300:9300 elasticsearch:8.15.2
+
+docker ps -a | findstr /C:"%CONTAINER_NAME%" >nul
+
+if errorlevel 1 (
+    echo Container [%CONTAINER_NAME%] not existed, creating...
+    %COMMAND%
+) else (
+    docker ps | findstr /C:"%CONTAINER_NAME%" >nul
+    if errorlevel 1 (
+        echo Container with the same name [%CONTAINER_NAME%] stopped, restarting...
+        docker start %CONTAINER_NAME%
+    ) else (
+        echo Container [%CONTAINER_NAME%] is already running...
+    )
+)
