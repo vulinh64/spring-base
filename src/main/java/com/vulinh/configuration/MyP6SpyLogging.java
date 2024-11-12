a94d41e0a2d69c05e14a150417bf98b5bc9d49cb
vu-linh <138560515+vu-linh@users.noreply.github.com>
2024-11-12 17:06:55 +0700

    P6Spy logging modification

--- src/main/java/com/vulinh/configuration/MyP6SpyLogging.java
+++ src/main/java/com/vulinh/configuration/MyP6SpyLogging.java
@@ -2,11 +2,31 @@ package com.vulinh.configuration;
 
 import com.p6spy.engine.logging.Category;
 import com.p6spy.engine.spy.appender.Slf4JLogger;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class MyP6SpyLogging extends Slf4JLogger {
+
+  private enum LogCategory {
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG,
+    BATCH,
+    STATEMENT,
+    RESULTSET,
+    COMMIT,
+    ROLLBACK,
+    RESULT,
+    OUTAGE;
+
+    static final Set<String> SETS =
+        Arrays.stream(LogCategory.values()).map(Enum::name).collect(Collectors.toSet());
+  }
 
   @Override
   public void logSQL(
@@ -17,19 +37,28 @@ public class MyP6SpyLogging extends Slf4JLogger {
       String prepared,
       String sql,
       String url) {
-    switch (category.getName().toUpperCase()) {
-      case "ERROR" -> log.error("(ERROR) {}", sql);
-      case "WARN" -> log.warn("(WARN)  {}", sql);
-      case "DEBUG" -> log.debug("(DEBUG) {}", sql);
-      case "TRACE" -> log.trace("(TRACE) {}", sql);
-      case "COMMIT" -> log.info("Connection #{} committed...", connectionId);
-      case "ROLLBACK" -> log.info("Connection #{} rolled back...", connectionId);
-      case "OUTAGE" -> log.warn("Connection #{} encountered outage...", connectionId);
-      default -> {
-        if (StringUtils.isNotBlank(sql)) {
-          log.info("\n\n{}\n",sql);
-        }
+    var categoryName = category.getName().toUpperCase();
+
+    if (StringUtils.isBlank(sql)) {
+      if (LogCategory.SETS.contains(categoryName)) {
+        log.info(
+            "#{} [{}] - {}",
+            "%04d".formatted(connectionId),
+            "%9s".formatted(categoryName),
+            switch (LogCategory.valueOf(categoryName)) {
+              case COMMIT -> "Transaction committed";
+              case ROLLBACK -> "Transaction rolled back";
+              case BATCH -> "Batch executed";
+              case OUTAGE -> "Connection outage occurred";
+              default -> "...";
+            });
+
+        return;
       }
+
+      super.logSQL(connectionId, time, elapsed, category, prepared, sql, url);
     }
+
+    log.info(sql);
   }
 }
