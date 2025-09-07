2cd577e1edf38041c8b58a305c44be736eb6f525
Nguyen Vu Linh <nguyenvulinh64@outlook.com>
2025-09-07 16:59:08 +0700

    No need for @EnableAsync, the async tasks will be executed via Spring Event

--- src/main/java/com/vulinh/SpringBaseProjectApplication.java
+++ src/main/java/com/vulinh/SpringBaseProjectApplication.java
@@ -9,13 +9,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = AuditorConfiguration.AUDITOR_PROVIDER)
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
-@EnableAsync
 @EnableScheduling
 @EnableConfigurationProperties(ApplicationProperties.class)
 @EnableCaching
