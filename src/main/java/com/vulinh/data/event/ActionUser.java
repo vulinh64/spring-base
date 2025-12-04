bdf14bd63a616293d43b17da8b5c242e8a9e55df
Linh Nguyen <nguyenvulinh64@outlook.com>
2025-12-04 14:42:03 +0700

    ActionUser is now Identifiable<UUID>

--- src/main/java/com/vulinh/data/event/ActionUser.java
+++ src/main/java/com/vulinh/data/event/ActionUser.java
@@ -2,9 +2,10 @@ package com.vulinh.data.event;
 
 import module java.base;
 
+import com.vulinh.data.base.RecordUuidIdentifiable;
 import lombok.Builder;
 import lombok.With;
 
 @Builder
 @With
-public record ActionUser(UUID id, String username) {}
+public record ActionUser(UUID id, String username) implements RecordUuidIdentifiable {}
