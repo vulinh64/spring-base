4a7b3e8218c3b9d29a673206e1aaf98263be9d69
Linh Nguyen <nguyenvulinh64@outlook.com>
2025-12-24 17:26:33 +0700

    Remove @EnableScheduling

--- src/main/java/com/vulinh/SpringBaseProjectApplication.java
+++ src/main/java/com/vulinh/SpringBaseProjectApplication.java
@@ -11,12 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = AuditorConfiguration.AUDITOR_PROVIDER)
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
-@EnableScheduling
 @EnableConfigurationProperties(ApplicationProperties.class)
 @EnableAsync
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
