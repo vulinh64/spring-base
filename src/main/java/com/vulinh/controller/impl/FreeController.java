2389ae90e88be1b604f68b45428272c750c17feb
Linh Nguyen <nguyenvulinh64@outlook.com>
2026-01-02 17:32:09 +0700

    Clean up time

--- src/main/java/com/vulinh/controller/impl/FreeController.java
+++ src/main/java/com/vulinh/controller/impl/FreeController.java
@@ -6,9 +6,7 @@ import com.vulinh.controller.api.FreeAPI;
 import com.vulinh.data.dto.response.GenericResponse.ResponseCreator;
 import com.vulinh.service.taxcalculator.TaxRequest;
 import com.vulinh.service.taxcalculator.TaxService;
-import com.vulinh.utils.springcron.dto.SpringCronGeneratorDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,11 +31,5 @@ public class FreeController implements FreeAPI {
                 .isProbation(isProbation)
                 .probationPercentage(probationPercentage)
                 .build()));
-  }
-
-  @Override
-  public Object generateSpringCronExpression(
-      @NonNull SpringCronGeneratorDTO springCronGeneratorDTO) {
-    return Map.of("expression", springCronGeneratorDTO.toCronExpression());
   }
 }
